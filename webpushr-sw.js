4cad4e6abdce09c39cefc22a874f239dc0dbd41a
andy <rollinghell@gmail.com>
2021-10-23 12:58:51 +0800

    Site updated: 2021-10-23 12:58:48

--- webpushr-sw.js
+++ webpushr-sw.js
@@ -1,1 +1,1 @@
-importScripts('https://cdn.webpushr.com/sw-server.min.js');<link rel="stylesheet" href="/css/bilicard.css" type="text/css">
+importScripts('https://cdn.webpushr.com/sw-server.min.js');
