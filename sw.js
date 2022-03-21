6c644ccf67be5869391d117eae8dc65e87ef2f93
andy <rollinghell@gmail.com>
2022-03-22 00:47:07 +0800

    Site updated: 2022-03-22 00:45:02

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 使用{uniqueIdentifier}模板，稍后我们将使用hexo的事件机制，替换成ISO时间，作为每次构建的唯一标识符
-var cacheName = '17lai-cache-20220322000844';
+var cacheName = '17lai-cache-20220322004344';
 // 在这个数组里面写入您主页加载需要的资源文件
 var filesToCache = [
   '/js/matery.js',
