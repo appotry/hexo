10a6416d3a9378fba7bcf62801b3c41ba110c486
appotry <andycrusoe@gmail.com>
2022-04-29 23:41:38 +0800

    Site updated: 2022-04-29 23:37:52

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,1 +1,1 @@
-$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+$((function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')}));
