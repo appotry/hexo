f2fe3f74745c5e78f86bc746e66f551a033491d6
appotry <andycrusoe@gmail.com>
2022-09-27 21:08:17 +0800

    Site updated: 2022-09-27 21:08:13

---
+++ sw.js
@@ -0,0 +1,61 @@
+// 使用{uniqueIdentifier}模板，稍后我们将使用hexo的事件机制，替换成ISO时间，作为每次构建的唯一标识符
+var cacheStorageKey = '17lai-cache-20220927210332';
+// 在这个数组里面写入您主页加载需要的资源文件
+var cacheList = [
+  '/css/matery.css',
+  '/css/my.css',
+  '/css/post.css',
+  '/css/indexcover.css',
+
+  '/js/img-lazyload.js',
+  '/js/color-schema.js',
+  '/js/plugins.js',
+  '/js/boot.js',
+  '/js/utils.js',
+  '/js/events.js'
+];
+
+// self.addEventListener('install', e => {
+//   e.waitUntil(
+//     caches.open(cacheStorageKey).then(cache => {
+//       return cache.addAll(cacheList)
+//         .then(() => self.skipWaiting());
+//     })
+//   );
+// });
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+    .then(cache => cache.addAll(cacheList))
+    .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== cacheStorageKey) {
+          // 清理旧版本
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  // 更新客户端
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheStorageKey)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+        // 使用缓存而不是进行网络请求，实现app秒开
+        return response || fetch(event.request);
+      })
+  );
+});
