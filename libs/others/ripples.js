f0de2cbe5d8f4d0f77d9b92fa08bec0e4589b4ad
appotry <andycrusoe@gmail.com>
2023-02-07 15:26:50 +0800

    Site updated: 2023-02-07 15:26:08

--- libs/others/ripples.js
+++ libs/others/ripples.js
@@ -1,16 +1,1 @@
-$(document).ready(function () {
-  try {
-    $("#banner").ripples({
-      resolution: 512,
-      dropRadius: 10, //px
-      perturbance: 0.04,
-    });
-    $("#banner .carousel-item").ripples({
-      resolution: 512,
-      dropRadius: 10, //px
-      perturbance: 0.04,
-    });
-  } catch (e) {
-    $(".error").show().text(e);
-  }
-});
+$(document).ready(function(){try{$("#banner").ripples({resolution:512,dropRadius:10,perturbance:.04}),$("#banner .carousel-item").ripples({resolution:512,dropRadius:10,perturbance:.04})}catch(r){$(".error").show().text(r)}});
