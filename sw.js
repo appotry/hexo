bd138a4258b956f9e292587c48bfccfbbc8131a0
appotry <andycrusoe@gmail.com>
2023-02-12 22:23:46 +0800

    Site updated: 2023-02-12 22:22:43

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
 // 使用{uniqueIdentifier}模板，稍后我们将使用hexo的事件机制，替换成ISO时间，作为每次构建的唯一标识符
-var cacheStorageKey = '17lai-cache-20230212173820';
+var cacheStorageKey = '17lai-cache-20230212220843';
 // 在这个数组里面写入您主页加载需要的资源文件
 var cacheList = [
-  '{cdnUrl}/css/matery.css',
-  '{cdnUrl}/css/my.css',
-  '{cdnUrl}/css/post.css',
-  '{cdnUrl}/css/reward.css',
-  '{cdnUrl}/css/waline.css',
+  'https://fast.17lai.site/css/matery.css',
+  'https://fast.17lai.site/css/my.css',
+  'https://fast.17lai.site/css/post.css',
+  'https://fast.17lai.site/css/reward.css',
+  'https://fast.17lai.site/css/waline.css',
   '/css/highlight.css',
   '/css/highlight-dark.css',
 
-  '{cdnUrl}/js/color-schema.js',
-  '{cdnUrl}/js/plugins.js',
-  '{cdnUrl}/js/tw_cn.js',
-  '{cdnUrl}/js/boot.js',
-  '{cdnUrl}/js/utils.js',
-  '{cdnUrl}/js/events.js'
+  'https://fast.17lai.site/js/color-schema.js',
+  'https://fast.17lai.site/js/plugins.js',
+  'https://fast.17lai.site/js/tw_cn.js',
+  'https://fast.17lai.site/js/boot.js',
+  'https://fast.17lai.site/js/utils.js',
+  'https://fast.17lai.site/js/events.js'
 ];
 
 // self.addEventListener('install', e => {
