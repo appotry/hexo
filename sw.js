8b5d3b94636fe50dda6dd1ebf815a8c36761a6ba
appotry <andycrusoe@gmail.com>
2023-02-18 23:15:23 +0800

    Site updated: 2023-02-18 23:14:49

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 使用{uniqueIdentifier}模板，稍后我们将使用hexo的事件机制，替换成ISO时间，作为每次构建的唯一标识符
-var cacheStorageKey = '17lai-cache-20230212225337';
+var cacheStorageKey = '17lai-cache-20230218230516';
 // 在这个数组里面写入您主页加载需要的资源文件
 var cacheList = [
   'https://fast.17lai.site/css/matery.css',
@@ -9,6 +9,17 @@ var cacheList = [
   'https://fast.17lai.site/css/waline.css',
   '/css/highlight.css',
   '/css/highlight-dark.css',
+
+  'https://fast.17lai.site/libs/jquery/jquery.min.js',
+  'https://fast.17lai.site/libs/jquery/jquery-ui.min.css',
+  'https://fast.17lai.site/libs/jquery/jquery-ui.min.js',
+  'https://fast.17lai.site/libs/materialize/materialize.min.js',
+  'https://fast.17lai.site/libs/materialize/materialize.min.css',
+  'https://fast.17lai.site/libs/waline/waline.min.js',
+  'https://fast.17lai.site/libs/waline/waline.min.css',
+  'https://fast.17lai.site/libs/waline/comment.min.mjs',
+  'https://fast.17lai.site/libs/waline/pageview.min.mjs',
+  'https://fast.17lai.site/libs/waline/waline-count.js',
 
   'https://fast.17lai.site/js/color-schema.js',
   'https://fast.17lai.site/js/plugins.js',
