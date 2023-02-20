72a24576ecc2839a8809f726823e32bd4864277c
appotry <andycrusoe@gmail.com>
2023-02-20 16:08:54 +0800

    Site updated: 2023-02-20 16:07:49

--- sw.js
+++ sw.js
@@ -1,32 +1,32 @@
 // 使用{uniqueIdentifier}模板，稍后我们将使用hexo的事件机制，替换成ISO时间，作为每次构建的唯一标识符
-var cacheStorageKey = '17lai-cache-20230218230516';
+var cacheStorageKey = '17lai-cache-20230220155430';
 // 在这个数组里面写入您主页加载需要的资源文件
 var cacheList = [
-  'https://fast.17lai.site/css/matery.css',
-  'https://fast.17lai.site/css/my.css',
-  'https://fast.17lai.site/css/post.css',
-  'https://fast.17lai.site/css/reward.css',
-  'https://fast.17lai.site/css/waline.css',
+  '/css/matery.css',
+  '/css/my.css',
+  '/css/post.css',
+  '/css/reward.css',
+  '/css/waline.css',
   '/css/highlight.css',
   '/css/highlight-dark.css',
 
-  'https://fast.17lai.site/libs/jquery/jquery.min.js',
-  'https://fast.17lai.site/libs/jquery/jquery-ui.min.css',
-  'https://fast.17lai.site/libs/jquery/jquery-ui.min.js',
-  'https://fast.17lai.site/libs/materialize/materialize.min.js',
-  'https://fast.17lai.site/libs/materialize/materialize.min.css',
-  'https://fast.17lai.site/libs/waline/waline.min.js',
-  'https://fast.17lai.site/libs/waline/waline.min.css',
-  'https://fast.17lai.site/libs/waline/comment.min.mjs',
-  'https://fast.17lai.site/libs/waline/pageview.min.mjs',
-  'https://fast.17lai.site/libs/waline/waline-count.js',
+  '/libs/jquery/jquery.min.js',
+  '/libs/jquery/jquery-ui.min.css',
+  '/libs/jquery/jquery-ui.min.js',
+  '/libs/materialize/materialize.min.js',
+  '/libs/materialize/materialize.min.css',
+  '/libs/waline/waline.min.js',
+  '/libs/waline/waline.min.css',
+  '/libs/waline/comment.min.mjs',
+  '/libs/waline/pageview.min.mjs',
+  '/libs/waline/waline-count.js',
 
-  'https://fast.17lai.site/js/color-schema.js',
-  'https://fast.17lai.site/js/plugins.js',
-  'https://fast.17lai.site/js/tw_cn.js',
-  'https://fast.17lai.site/js/boot.js',
-  'https://fast.17lai.site/js/utils.js',
-  'https://fast.17lai.site/js/events.js'
+  '/js/color-schema.js',
+  '/js/plugins.js',
+  '/js/tw_cn.js',
+  '/js/boot.js',
+  '/js/utils.js',
+  '/js/events.js'
 ];
 
 // self.addEventListener('install', e => {
