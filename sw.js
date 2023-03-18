a4248ce728584d5205fbefe193ec474bbfceeb98
appotry <andycrusoe@gmail.com>
2023-03-18 23:16:45 +0800

    Site updated: 2023-03-18 23:15:47

--- sw.js
+++ sw.js
@@ -1,32 +1,32 @@
 // 使用{uniqueIdentifier}模板，稍后我们将使用hexo的事件机制，替换成ISO时间，作为每次构建的唯一标识符
-var cacheStorageKey = '17lai-cache-20230318203410';
+var cacheStorageKey = '17lai-cache-20230318230424';
 // 在这个数组里面写入您主页加载需要的资源文件
 var cacheList = [
-  '/css/matery.css',
-  '/css/my.css',
-  '/css/post.css',
-  '/css/reward.css',
-  '/css/waline.css',
-  '/css/highlight.css',
-  '/css/highlight-dark.css',
+  '/css/matery.css?v=1.0.0',
+  '/css/my.css?v=1.0.0',
+  '/css/post.css?v=1.0.0',
+  '/css/reward.css?v=1.0.0',
+  '/css/waline.css?v=1.0.0',
+  '/css/highlight.css?v=1.0.0',
+  '/css/highlight-dark.css?v=1.0.0',
 
   '/libs/jquery/jquery.min.js',
   '/libs/jquery/jquery-ui.min.css',
   '/libs/jquery/jquery-ui.min.js',
-  '/libs/materialize/materialize.min.js',
-  '/libs/materialize/materialize.min.css',
-  '/libs/waline/waline.min.js',
-  '/libs/waline/waline.min.css',
+  '/libs/materialize/materialize.min.js?v=1.2.1 ',
+  '/libs/materialize/materialize.min.css?v=1.2.1 ',
+  '/libs/waline/waline.min.js?v=2.14.9',
+  '/libs/waline/waline.min.css?v=2.14.9',
   '/libs/waline/comment.min.mjs',
   '/libs/waline/pageview.min.mjs',
   '/libs/waline/waline-count.js',
 
-  '/js/color-schema.js',
-  '/js/plugins.js',
-  '/js/tw_cn.js',
-  '/js/boot.js',
-  '/js/utils.js',
-  '/js/events.js'
+  '/js/color-schema.js?v=1.0.0',
+  '/js/plugins.js?v=1.0.0',
+  '/js/tw_cn.js?v=1.0.0',
+  '/js/boot.js?v=1.0.0',
+  '/js/utils.js?v=1.0.0',
+  '/js/events.js?v=1.0.0'
 ];
 
 // self.addEventListener('install', e => {
