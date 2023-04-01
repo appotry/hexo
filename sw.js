d598d4d0270d1e2e99eb93c9c9dda4d699c9d259
appotry <andycrusoe@gmail.com>
2023-04-02 05:50:29 +0800

    Site updated: 2023-04-02 05:50:17

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 使用{uniqueIdentifier}模板，稍后我们将使用hexo的事件机制，替换成ISO时间，作为每次构建的唯一标识符
-var cacheStorageKey = '17lai-cache-20230401005328';
+var cacheStorageKey = '17lai-cache-20230402054228';
 // 在这个数组里面写入您主页加载需要的资源文件
 var cacheList = [
   '/css/matery.css?v=1.0.2',
@@ -10,8 +10,11 @@ var cacheList = [
   '/libs/jquery/jquery.min.js',
   '/libs/jquery/jquery-ui.min.css',
   '/libs/jquery/jquery-ui.min.js',
-  '/libs/materialize/materialize.min.js?v=1.2.1 ',
-  '/libs/materialize/materialize.min.css?v=1.2.1 ',
+  '/libs/materialize/materialize.min.js?v=1.2.2',
+  '/libs/materialize/materialize.min.css?v=1.2.2',
+  '/libs/masonry/masonry.pkgd.min.js',
+  '/libs/aos/aos.min.css',
+  '/libs/aos/aos.min.js',
   '/libs/waline/comment.min.mjs',
   '/libs/waline/pageview.min.mjs',
   '/libs/waline/waline-count.js',
