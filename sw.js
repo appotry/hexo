8553f255cf7a7517efe9375ecf882f279373b5e9
appotry <andycrusoe@gmail.com>
2024-03-28 08:53:07 +0800

    Site updated: 2024-03-28 08:52:18

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 使用{uniqueIdentifier}模板，稍后我们将使用hexo的事件机制，替换成ISO时间，作为每次构建的唯一标识符
-var cacheStorageKey = '17lai-cache-20240319175139';
+var cacheStorageKey = '17lai-cache-20240328084143';
 // 在这个数组里面写入您主页加载需要的资源文件
 var cacheList = [
   '/css/matery.css?v=1.0.9',
@@ -19,6 +19,7 @@ var cacheList = [
   '/libs/waline/pageview.mjs',
   '/libs/waline/waline-count.js',
 
+  '/js/umami.js?v=1.0.0',
   '/js/color-schema.js?v=1.0.0',
   '/js/plugins.js?v=1.0.0',
   '/js/tw_cn.js?v=1.0.0',
@@ -68,6 +69,10 @@ self.addEventListener('fetch', event => {
       .then(response => {
         // 使用缓存而不是进行网络请求，实现app秒开
         return response || fetch(event.request);
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+        throw error;
       })
   );
 });
