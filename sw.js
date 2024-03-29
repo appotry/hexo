98793764737d8904a60dfdc530f4c4953c062cd2
appotry <andycrusoe@gmail.com>
2024-03-29 08:18:36 +0800

    Site updated: 2024-03-29 08:18:08

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 使用{uniqueIdentifier}模板，稍后我们将使用hexo的事件机制，替换成ISO时间，作为每次构建的唯一标识符
-var cacheStorageKey = '17lai-cache-20240328084143';
+var cacheStorageKey = '17lai-cache-20240329080858';
 // 在这个数组里面写入您主页加载需要的资源文件
 var cacheList = [
   '/css/matery.css?v=1.0.9',
@@ -15,11 +15,9 @@ var cacheList = [
   '/libs/masonry/masonry.pkgd.min.js',
   '/libs/aos/aos.min.css',
   '/libs/aos/aos.min.js',
-  '/libs/waline/comment.mjs',
-  '/libs/waline/pageview.mjs',
   '/libs/waline/waline-count.js',
 
-  '/js/umami.js?v=1.0.0',
+  '/js/umami.js?v=1.0.1',
   '/js/color-schema.js?v=1.0.0',
   '/js/plugins.js?v=1.0.0',
   '/js/tw_cn.js?v=1.0.0',
