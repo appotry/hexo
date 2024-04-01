cc12795e768503a45784e9928c42304d453ee625
appotry <andycrusoe@gmail.com>
2024-04-02 00:17:31 +0800

    Site updated: 2024-04-02 00:16:30

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // 使用{uniqueIdentifier}模板，稍后我们将使用hexo的事件机制，替换成ISO时间，作为每次构建的唯一标识符
-var cacheStorageKey = '17lai-cache-20240329190729';
+var cacheStorageKey = '17lai-cache-20240402000644';
 // 在这个数组里面写入您主页加载需要的资源文件
 var cacheList = [
-  '/css/matery.css?v=1.0.9',
+  '/css/matery.css?v=1.0.10',
   '/css/my.css?v=1.0.1',
   '/css/highlight.css?v=1.0.0',
   '/css/highlight-dark.css?v=1.0.0',
