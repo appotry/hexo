f1dd8eaad9c9dfe4611075b2aa20a90578e5a247
appotry <andycrusoe@gmail.com>
2024-04-05 12:56:13 +0800

    Site updated: 2024-04-05 12:55:26

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 使用{uniqueIdentifier}模板，稍后我们将使用hexo的事件机制，替换成ISO时间，作为每次构建的唯一标识符
-var cacheStorageKey = '17lai-cache-20240405014038';
+var cacheStorageKey = '17lai-cache-20240405124533';
 // 在这个数组里面写入您主页加载需要的资源文件
 var cacheList = [
   '/css/matery.css?v=1.0.11',
@@ -15,7 +15,7 @@ var cacheList = [
   '/libs/aos/aos.min.js',
   '/libs/waline/waline-count.js',
 
-  '/js/umami.js?v=1.0.1',
+  '/js/umami.js?v=1.0.2',
   '/js/color-schema.js?v=1.0.0',
   '/js/plugins.js?v=1.0.0',
   '/js/tw_cn.js?v=1.0.0',
