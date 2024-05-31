763e6a67576e342e7e2833b0e5e632838c11839f
appotry <andycrusoe@gmail.com>
2024-05-31 20:17:52 +0800

    Site updated: 2024-05-31 20:17:13

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,6 +1,1 @@
-// 代码块功能依赖
-
-$(function () {
-//$(document).ready(function(){
-    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
--- libs/prism/components/prism-tsx.js
+++ libs/prism/components/prism-tsx.js
@@ -1,15 +1,1 @@
-(function (Prism) {
-	var typescript = Prism.util.clone(Prism.languages.typescript);
-	Prism.languages.tsx = Prism.languages.extend('jsx', typescript);
-
-	// doesn't work with TS because TS is too complex
-	delete Prism.languages.tsx['parameter'];
-	delete Prism.languages.tsx['literal-property'];
-
-	// This will prevent collisions between TSX tags and TS generic types.
-	// Idea by https://github.com/karlhorky
-	// Discussion: https://github.com/PrismJS/prism/issues/2594#issuecomment-710666928
-	var tag = Prism.languages.tsx.tag;
-	tag.pattern = RegExp(/(^|[^\w$]|(?=<\/))/.source + '(?:' + tag.pattern.source + ')', tag.pattern.flags);
-	tag.lookbehind = true;
-}(Prism));
+(function(e){var a=e.util.clone(e.languages.typescript);e.languages.tsx=e.languages.extend("jsx",a);delete e.languages.tsx["parameter"];delete e.languages.tsx["literal-property"];var t=e.languages.tsx.tag;t.pattern=RegExp(/(^|[^\w$]|(?=<\/))/.source+"(?:"+t.pattern.source+")",t.pattern.flags);t.lookbehind=true})(Prism);
